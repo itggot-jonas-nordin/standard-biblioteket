19595d290ca4711bc07620e54a64c1f9abdad1ae
itggot-jonas-nordin <jonas.nordin@itggot.se>
2018-04-25 14:44:49 +0200

    index of char

---
+++ lib/index_of_char.rb
@@ -0,0 +1,21 @@
+# str - The String to be checked with.
+# char - The String to check with.
+#
+# Examples
+#
+# 	index_of_char("Hej jag heter Erik", "E")
+#   # => 14
+#
+# Returnes the index of the character.
+
+
+def index_of_char(str, char)
+	i = 0
+	while i < str.length - 1
+		if char == str[i]
+			return i
+		end
+		i += 1
+	end
+	return nil
+end
