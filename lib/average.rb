6a51aa67c8d131b55496bfdef05fa207ced6ad48
itggot-jonas-nordin <jonas.nordin@itggot.se>
2018-04-25 14:09:00 +0200

    documentation added

--- lib/average.rb
+++ lib/average.rb
@@ -1,5 +1,15 @@
 require '../lib/sum.rb'
 
+# ary - The Array to ba averaged.
+#
+# Examples
+#
+# 	average([5, 10, 15])
+#   # => 10
+#
+# Returnes the averaged array.
+
 def average(ary)
 	return sum(ary) / ary.length
 end
+
